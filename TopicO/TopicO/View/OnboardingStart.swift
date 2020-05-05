6732bca25bc8bb5f0950b5aad1004e2a5e981839
Marcus Vinicius Vieira Badiale <marcus.badiale@gmail.com>
2020-05-05 16:21:57 -0300

    Finishing detailView and starting onboarding

---
+++ TopicO/TopicO/View/OnboardingStart.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardingStart.swift
+//  TopicO
+//
+//  Created by Marcus Vinicius Vieira Badiale on 05/05/20.
+//  Copyright © 2020 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import SwiftUI
+
+struct OnboardingStart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingStart_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingStart()
+    }
+}
