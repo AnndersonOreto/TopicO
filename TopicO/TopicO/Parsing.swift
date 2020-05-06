dfc608791681b9eabe4ca754fdf41619d10a5b43
Marcus Vinicius Vieira Badiale <marcus.badiale@gmail.com>
2020-05-06 16:28:38 -0300

    adding parsing file

---
+++ TopicO/TopicO/Parsing.swift
@@ -0,0 +1,9 @@
+//
+//  Parsing.swift
+//  TopicO
+//
+//  Created by Marcus Vinicius Vieira Badiale on 06/05/20.
+//  Copyright © 2020 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import Foundation
